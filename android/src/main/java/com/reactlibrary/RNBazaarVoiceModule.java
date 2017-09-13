665492b7f2f49f85b7e16ec023fab1b97ab3b645
Krisztiaan <krisz011@gmail.com>
2017-09-13 11:57:19 +0200

    Android hooks

--- android/src/main/java/com/reactlibrary/RNBazaarVoiceModule.java
+++ android/src/main/java/com/reactlibrary/RNBazaarVoiceModule.java
@@ -3,8 +3,6 @@ package com.reactlibrary;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 
 public class RNBazaarVoiceModule extends ReactContextBaseJavaModule {
 
