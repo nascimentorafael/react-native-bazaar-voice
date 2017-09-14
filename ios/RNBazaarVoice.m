360ccc11f4a9fb156107f951cb0335f3051485a2
Rafael Nascimento <rafael.nascimento@possible.com>
2017-09-14 16:48:12 +0200

    add promise and new parameters to send review

--- ios/RNBazaarVoice.m
+++ ios/RNBazaarVoice.m
@@ -1,4 +1,4 @@
-#import "RNBazaarVoice"
+#import "RNBazaarVoice.h"
 #import "RCTConvert.h"
 #import "RCTLog.h"
 #import "RCTUIManager.h"
@@ -16,7 +16,7 @@ RCT_EXPORT_MODULE()
     return dispatch_get_main_queue();
 }
 
-RCT_EXPORT_METHOD(getProductReviewsWithId:(NSString *)productId andLimit:(int)limit andOffset:(int)offset failureCallback:(RCTResponseErrorBlock)failureCallback successCallback:(RCTResponseSenderBlock)successCallback) {
+RCT_EXPORT_METHOD(getProductReviewsWithId:(NSString *)productId andLimit:(int)limit andOffset:(int)offset withResolver:(RCTPromiseResolveBlock)resolve andRejecter:(RCTResponseSenderBlock)reject) {
     BVReviewsTableView *reviewsTableView = [BVReviewsTableView new];
     BVReviewsRequest* request = [[BVReviewsRequest alloc] initWithProductId:productId limit:limit offset:offset];
     [reviewsTableView load:request success:^(BVReviewsResponse * _Nonnull response) {
@@ -24,20 +24,21 @@ RCT_EXPORT_METHOD(getProductReviewsWithId:(NSString *)productId andLimit:(int)li
         for (BVReview *review in response.results) {
             [reviews addObject:[self jsonFromReview:review]];
         }
-        successCallback(reviews);
+        resolve(reviews);
     } failure:^(NSArray<NSError *> * _Nonnull errors) {
-        failureCallback(errors[0]);
+        reject(errors);
     }];
 }
 
-RCT_EXPORT_METHOD(submitReview:(NSDictionary *)review fromProduct:(NSString *)productId andUser:(NSDictionary *)user failureCallback:(RCTResponseErrorBlock)failureCallback successCallback:(RCTResponseSenderBlock)successCallback) {
+RCT_EXPORT_METHOD(submitReview:(NSDictionary *)review fromProduct:(NSString *)productId andUser:(NSDictionary *)user withResolver:(RCTPromiseResolveBlock)resolve andRejecter:(RCTResponseSenderBlock)reject) {
     
-    NSLog(@"BVuser %@",user);
-    NSLog(@"BVreview %@",review);
+    //    NSLog(@"BVuser %@",user);
+    //    NSLog(@"BVreview %@",review);
     
     // User info
     NSString *userNickname = [user objectForKey:@"userNickname"];
-    //    NSString *userId = [user objectForKey:@"userId"];
+    NSString *locale = [user objectForKey:@"locale"];
+    NSString *userId = [user objectForKey:@"userId"];
     NSString *userEmail = [user objectForKey:@"userEmail"];
     bool sendEmailAlertWhenPublished = [user objectForKey:@"sendEmailAlertWhenPublished"];
     bool agreedToTermsAndConditions = [user objectForKey:@"agreedToTermsAndConditions"];
@@ -45,45 +46,35 @@ RCT_EXPORT_METHOD(submitReview:(NSDictionary *)review fromProduct:(NSString *)pr
     // Review info
     NSString *title = [review objectForKey:@"title"];
     NSString *text = [review objectForKey:@"text"];
+    int comfort = [[review valueForKey:@"comfort"] intValue];
+    int size = [[review valueForKey:@"size"] intValue];
     int rating = [[review valueForKey:@"rating"] intValue];
-    
-    NSLog(@"rating %d", rating);
+    int quality = [[review valueForKey:@"quality"] intValue];
+    bool isRecommended = [user objectForKey:@"isRecommended"];
     
     BVReviewSubmission* bvReview = [[BVReviewSubmission alloc] initWithReviewTitle:title
                                                                         reviewText:text
                                                                             rating:rating
                                                                          productId:productId];
     bvReview.action = BVSubmissionActionPreview;
+    bvReview.locale = locale;
     bvReview.userNickname = userNickname;
-    bvReview.user = [NSString stringWithFormat:@"userId%d", arc4random()];
+    bvReview.user = userId;
     bvReview.userEmail = userEmail;
     bvReview.sendEmailAlertWhenPublished = [NSNumber numberWithBool:sendEmailAlertWhenPublished];
     bvReview.agreedToTermsAndConditions  = [NSNumber numberWithBool:agreedToTermsAndConditions];
-    bvReview.isRecommended = [NSNumber numberWithBool:YES];
+    bvReview.isRecommended = [NSNumber numberWithBool:isRecommended];
+    bvReview.hostedAuthenticationEmail = userEmail;
+    [bvReview addRatingQuestion:@"Comfort" value:comfort];
+    [bvReview addRatingQuestion:@"Size" value:size];
+    [bvReview addRatingQuestion:@"Quality" value:quality];
     
     [bvReview submit:^(BVReviewSubmissionResponse * _Nonnull response) {
         // review submitted successfully! 🎉
-        successCallback(@[response]);
+        resolve(response);
     } failure:^(NSArray * _Nonnull errors) {
         // handle failure appropriately  🚨
-        for (NSError* error in errors) {
-            
-            // If fields had errors, useful information is available in the error
-            NSString* fieldErrorName = error.userInfo[BVFieldErrorName];
-            NSString* fieldErrorMessage = error.userInfo[BVFieldErrorMessage];
-            
-            if (fieldErrorName && [fieldErrorName isEqualToString:@"reviewText"]) {
-                // fieldErrorMessage = "You must enter review text."
-                NSLog(@"fieldErrorName: %@ - %@", fieldErrorName, fieldErrorMessage);
-            }
-            
-            if (fieldErrorName && [fieldErrorName isEqualToString:@"rating"]) {
-                // fieldErrorMessage = "This field must be between 1 and 5."
-                NSLog(@"fieldErrorName: %@ - %@", fieldErrorName, fieldErrorMessage);
-            }
-            
-        }
-        failureCallback(errors[0]);
+        reject(errors);
     }];
 }
 
